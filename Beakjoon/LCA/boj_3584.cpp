a2a616353e9477f4e2de229b5d158c42f9abe03b
hashilyze <hashilyze@gmail.com>
2024-05-24 23:37:55 +0900

    [Beakjoon] update: 3584

--- Beakjoon/LCA/boj_3584.cpp
+++ Beakjoon/LCA/boj_3584.cpp
@@ -2,6 +2,8 @@
 // https://www.acmicpc.net/problem/3584
 
 
+#include <algorithm>
+#include <cmath>
 #include <cstring>
 #include <iostream>
 #include <vector>
@@ -10,49 +12,50 @@
 #define FASTIO std::ios_base::sync_with_stdio(false); std::cin.tie(NULL); std::cout.tie(NULL);
 // types
 using ivec = std::vector<int>;
-
-struct Node {
-    Node() = default;
-    Node(int p, int d) : parent(p), depth(d) { }
-    int parent = -1;
-    int depth = -1;
-};
 // constants
 constexpr int MAX_N = 10'000;
+const int MAX_LEVEL = std::floor(std::log2(MAX_N)) + 0.1;
 // variables
 int n;
-ivec adj[MAX_N + 1];
-Node nodes[MAX_N + 1];
+ivec graph[MAX_N + 1];
+int depth[MAX_N + 1] = { -1, };
+int parents[MAX_N + 1][20];
 
+
+void build(int u, int p){
+    depth[u] = depth[p] + 1;
+    parents[u][0] = p;
+    
+    for(int i = 1; i <= MAX_LEVEL; ++i){
+        parents[u][i] = parents[parents[u][i - 1]][i - 1];
+    }
+    for(int v : graph[u]){
+        build(v, u);
+    }
+}
 
 int LCA(int u, int v){
-    if(u == -1 || v == -1) return -1;
-
-    const Node &nodeU = nodes[u], &nodeV = nodes[v];
-    if(nodeU.depth < nodeV.depth){
-        return LCA(u, nodeV.parent);
-    } else if(nodeU.depth > nodeV.depth){
-        return LCA(nodeU.parent, v);
-    } else{
-        if(u == v) return u;
-        else return LCA(nodeU.parent, nodeV.parent);
-    }
-}
-
-
-void dfs(int u, int d){
-    for(int v : adj[u]) {
-        nodes[v] = {u, d + 1};
-        dfs(v, d + 1);
-    }
-}
-void dfsAll(){
-    for(int u = 1; u <= n; ++u) {
-        if(nodes[u].depth == -1) {
-            nodes[u] = {u, 1};
-            dfs(u, 1);
+    if(depth[u] != depth[v]){
+        if(depth[u] > depth[v]) std::swap(u, v);
+        int b = depth[v] - depth[u];
+        for(int i = 0; i <= MAX_LEVEL; ++i){
+            if((b >> i) & 1){
+                v = parents[v][i];
+            }
         }
     }
+
+    int lca = u;
+    if(u != v){
+        for(int i = MAX_LEVEL; i >= 0; --i){
+            if(parents[u][i] != parents[v][i]){
+                u = parents[u][i];
+                v = parents[v][i];
+            }
+        }
+        lca = parents[u][0];
+    }
+    return lca;
 }
 
 int main(void){
@@ -61,21 +64,26 @@ int main(void){
     int t;
     std::cin >> t;
     while(t--){
-        // init.
-        for(auto &vec : adj) vec.clear();
-        std::memset(nodes, 0xFF, sizeof(nodes));
-        // input.
-        std::cin >> n;
+        std::memset(depth, 0x00, sizeof(depth)); depth[0] = -1;
+        std::memset(parents, 0x00, sizeof(parents));
+        for(int i = 1; i <= MAX_N; ++i) graph[i].clear();
+
         int u, v;
+        std::cin >> n;
         for(int i = 1; i < n; ++i){
             std::cin >> u >> v;
-            adj[u].push_back(v);
+            graph[u].push_back(v);
+            parents[v][0] = u;
         }
-        dfsAll();
-        // output.
+        for(int i = 1; i <= MAX_N; ++i){
+            if(parents[i][0] == 0){
+                build(i, 0);
+                break;
+            }
+        }
+        
         std::cin >> u >> v;
-        int lca = LCA(u, v);
-        std::cout << lca << '\n';
+        std::cout << LCA(u, v) << '\n';
     }
 
     return 0;
