04c22e2f411189c6f6fdfc23a12f542c8b86c276
JUNSU BAE <jsbae1023@gmail.com>
2025-01-12 13:56:39 +0900

    [Beakjoon] 11653. 소인수분해

--- Beakjoon/Number Theory/boj_11653.cpp
+++ Beakjoon/Number Theory/boj_11653.cpp
@@ -1,7 +1,6 @@
 // Beakjoon 11653 - 소인수분해
 // https://www.acmicpc.net/problem/11653
 
-#include <cstring>
 #include <iostream>
 #include <vector>
 
@@ -9,40 +8,20 @@
 #define FASTIO std::ios::sync_with_stdio(0); std::cin.tie(0);
 // types
 // constants
-constexpr int SIZE = 10'000'000;
 // variables
-bool isPrimes[SIZE + 1];
-
-
-void init(){
-    // 소수 배열 초기화
-    std::memset(isPrimes, true, sizeof(isPrimes));
-    isPrimes[0] = isPrimes[1] = false;
-    for(int p = 2; p * p <= SIZE; ++p){
-        if(!isPrimes[p]) continue;
-        for(int n = p * p; n <= SIZE; n += p){
-            isPrimes[n] = false;
-        }
-    }
-}
 
 void solution(int n, std::vector<int>& outAns){
     // 소인수 분해
-    int p = 2;
-    while(n > 1){ 
-        int flag = n % p;
-        if (n % p == 0) {
+    for(int p = 2; p <= n; ++p){
+        while(n % p == 0){
             n /= p;
             outAns.push_back(p);
-        } else {
-            while(!isPrimes[++p]) continue;
         }
     }
 }
 
 int main(void){
     FASTIO
-    init();
 
     int N;
     std::cin >> N;
